3f50c377b1df78de7e74dbdd6d9a66b1d48c5d50
durga1808 <137372215+durga1808@users.noreply.github.com>
2023-07-21 15:46:29 +0530

    Update Thread.java

--- Thread.java
+++ Thread.java
@@ -2,9 +2,9 @@ public class Thread {
   public static void main(String[] args) {
     Main thread = new Main();
     thread.start();
-    System.out.println("This code is outside of the thread");
+    System.out.println(" code is outside of the thread....");
   }
   public void run() {
-    System.out.println("This code is running in a thread");
+    System.out.println("code is running in a thread....");
   }
 }
